956b1412cf5cea61d01fa1fb14fbbdf865d491e6
Stiven Manco Valencia <1152457543@medellin.gov.co>
2024-11-05 10:56:52 -0500

    primer commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,57 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [usuario, setUsuario] = useState("");
+  const [numeroClicks, setNumeroClicks] = useState(0);
+
+  const reqApi = async () => {
+    const usuarioInfo = {
+      usuario: usuario,
+      numeroClicks: numeroClicks,
+    };
+    console.log(usuarioInfo);
+    const api = await fetch("http://localhost:9001/api/app_dummy", {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(usuarioInfo),
+    });
+    console.log(api);
+  };
+
+  const reiniciarContador = () => {
+    setNumeroClicks((numeroClicks) => 0);
+    document.getElementById("nombre").value = ``;
+  };
+
+  const fuctions = () => {
+    reqApi();
+    reiniciarContador();
+  };
 
   return (
     <>
       <h1>Hola Mundo</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <input
+          name="nombre"
+          id="nombre"
+          type="text"
+          onChange={(event) => setUsuario(event.target.value)}
+        />
+      </div>
+      <div className="card">
+        <button
+          onClick={() => setNumeroClicks((numeroClicks) => numeroClicks + 1)}
+        >
+          count is {numeroClicks}
         </button>
+        <button onClick={fuctions}>Guardar</button>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
